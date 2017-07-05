e9236604520f18ee337a9632df33c952e155b1c1
731614105013 <gomathiammupk29@gmail.com>
2017-07-05 15:05:12 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,22 @@
+# odd-intervals
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Odd
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int u;
+    for(int j=0;j<n;j++){
+      if(j%2!=0)
+      {
+        System.out.println("The numbers are:"+j);
+        
+      }
+      
+    }
+  }
+}
+      
